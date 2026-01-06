a6e178f4d78e72ba1199be20b947db518089f7f2
Samrat-Debugger <md721023@gmail.com>
2026-01-07 00:17:58 +0600

    proposal updated

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const h1 = document.querySelector("h1");
 const yes = document.querySelector("#yes");
 const no = document.querySelector("#no");
 
+yes.addEventListener("click", () => {
+  h1.innerText = "Love You Too Babe";
+  no.style.display = "none";
+});
+
 no.addEventListener("mouseover", (e) => {
   let randomValOne = Math.round(Math.random() * 300);
   let randomValTwo = Math.round(Math.random() * 300);

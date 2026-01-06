248d4f28b5087244dc43fbeb6846381ccc0411dc
Samrat-Debugger <md721023@gmail.com>
2026-01-06 23:58:29 +0600

    proposal done

---
+++ app.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector("h1");
+const yes = document.querySelector("#yes");
+const no = document.querySelector("#no");
+
+no.addEventListener("mouseover", (e) => {
+  let randomValOne = Math.round(Math.random() * 300);
+  let randomValTwo = Math.round(Math.random() * 300);
+  e.target.style.left = `${randomValOne}px`;
+  e.target.style.top = `${randomValTwo}px`;
+});
